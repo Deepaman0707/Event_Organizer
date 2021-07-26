fe0cb3fe68afaa3022dcd2a5c6f6fcde27be6ed9
Akshatg625 <akshatg625@gmail.com>
2021-07-26 10:57:47 +0530

    Login Page Done

--- src/components/AuthServices/LoginPage.js
+++ src/components/AuthServices/LoginPage.js
@@ -1,92 +1,165 @@
-import React, {useState} from 'react';
-import { connect } from 'react-redux';
-import {startLogin } from './../../actions/auth';
-import isEmail from 'validator/lib/isEmail';
-import Link from 'react-router-dom/Link';
-import Tilt from 'react-tilt';
-import { faGoogle, faFacebookSquare} from "@fortawesome/free-brands-svg-icons";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { startLogin } from "./../../actions/auth";
+import isEmail from "validator/lib/isEmail";
+import Link from "react-router-dom/Link";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import ParticlesBg from "particles-bg";
+
+import { faGoogle, faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export const LoginPage = ({ startLogin, error,unsetError, loading}) => {
 
-    const [email, getEmail] = useState('');
-    const [password, getPassword] = useState('');
-    const onStartLogin = (e) => {
-        e.preventDefault(); 
-        if(isEmail(email)){
-            const credentials = {
-                email,
-                password
-            };
-            
-            startLogin(credentials).then(() => {
-            })
-        }
-    }
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-    const showErrors = () => {
-        if(!!error)
-            alert(error);
-            unsetError();
+export const LoginPage = ({ startLogin, error, unsetError, loading }) => {
+  const classes = useStyles();
+
+  const [email, getEmail] = useState("");
+  const [password, getPassword] = useState("");
+
+  const onStartLogin = (e) => {
+    e.preventDefault();
+    if (isEmail(email)) {
+      const credentials = {
+        email,
+        password,
+      };
+
+      startLogin(credentials).then(() => {});
     }
-    return(
-        <div className="box-layout">
-            <div className="box-layout__logo-outside animated fadeInDown delay-1s">
-            </div>
-            {loading && <div className="spinner"></div>}
-            <div className="box-layout__box  animated fadeInRight delay-1s">
-            <div className="box-layout__logo-inside">
-                    <Tilt className="Tilt" options={{ max : 25 }} >
-                        <img src="/images/logo.png" alt=""/>
-                        <h1 className="box-layout__title animated flash delay-2s">Occasionly</h1>
-                        <h2 className="box-layout__subtitle">Explore different activities held in RAIT</h2>
-                    </Tilt>
-            </div>
-                <div className="box-layout__form">
-                    <form onSubmit={onStartLogin}>
-                        <input  type="email" value={email}
-                        onChange={e => (getEmail(e.target.value))}
-                        placeholder="email"
-                        className="animated fadeInLeft delay-2s"
-                        />
-                        <input type="password" value={password}  
-                        onChange={e => (getPassword(e.target.value))}
-                        placeholder={error ? <p>{error}</p> : "password"}
-                        className="animated fadeInRight delay-2s"
-                        />
-                        <button className="button button-primary">Login</button>    
-                        {/* <h5>or login using</h5> */}
-                        {/* <div className="box-layout__signin"> 
-                            <button>
-                                <FontAwesomeIcon icon={faGoogle}size="3x"/>
-                            </button> 
-                            <button>
-                                <FontAwesomeIcon icon={faFacebookSquare}  size="3x"/>
-                            </button>
-                            
-                        </div> */}
-                    </form>
-                    {error && showErrors()}
-                    <div className="box-layout__question">
-                    <label className="animated infinite pulse">no account? </label><Link to='/signup'>Sign Up</Link>
-                    </div>
-                    
-                </div>
-            </div>
+  };
+
+  const showErrors = () => {
+    if (!!error) alert(error);
+    unsetError();
+  };
+  return (
+    <div>
+      {loading && <div className="spinner"></div>}
+      <Container className="main" component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={onStartLogin} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => getEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => getPassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-    )
-}
+      </Container>
+      <ParticlesBg type="random" bg={true} />
+      {/* <h5>or login using</h5>
+                        <div className="box-layout__signin"> 
+                             <button>
+                                 <FontAwesomeIcon icon={faGoogle}size="3x"/>
+                             </button> 
+                             <button>
+                                 <FontAwesomeIcon icon={faFacebookSquare}  size="3x"/>
+                             </button>
+                            
+                         </div> */}
+      {error && showErrors()}
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: (credentials)=> dispatch(startLogin(credentials)),
-    unsetError: () => dispatch({
-        type: 'SET_ERRORS',
-        error: ''
-    })
+  startLogin: (credentials) => dispatch(startLogin(credentials)),
+  unsetError: () =>
+    dispatch({
+      type: "SET_ERRORS",
+      error: "",
+    }),
 });
 
 const mapStateToProps = (state) => ({
-    error: state.auth.error,
-    loading: state.auth.loading
-})
+  error: state.auth.error,
+  loading: state.auth.loading,
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
